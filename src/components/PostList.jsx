8383f91f4bcdac2bbe498aeac036d8a957bcd80e
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-03-12 23:22:49 +0300

    added TransitionGroup

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import PostItem from './PostItem';
+import React from "react";
+import PostItem from "./PostItem";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-const PostList = ({posts, title, deletePost}) => {
-    if (!posts.length) {
-        return (
-            <h3 style={{ textAlign: "center" }}>Посты не найдены</h3>
-        )
-    }
+const PostList = ({ posts, title, deletePost }) => {
+  if (!posts.length) {
+    return <h3 style={{ textAlign: "center" }}>Посты не найдены</h3>;
+  }
 
-    return (
-        <div>
-            <h2 style={{ textAlign: "center" }}>
-                {title}
-            </h2>
-            {posts.map((post, index) => (
-                <PostItem number={index + 1} post={post} deletePost={deletePost} id={post.id} key={post.id} />
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>{title}</h2>
+      <TransitionGroup>
+        {posts.map((post, index) => 
+            <CSSTransition
+                key={post.id}
+                timeout={500}
+                classNames="post"   
+            >
+                <PostItem
+                    number={index + 1}
+                    post={post}
+                    deletePost={deletePost}
+                    id={post.id}
+                />
+            </CSSTransition>
+        )}
+        
+      </TransitionGroup>
+    </div>
+  );
 };
 
 export default PostList;
