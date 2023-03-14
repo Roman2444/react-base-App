c9724252d27f49bf43e193441fbe3c76913f9a93
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-03-14 19:24:05 +0300

    corrected PostIdPage

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import PostService from "../API/PostService";
+import Loader from "../components/UI/Loader/Loader";
+import { useFetching } from "../hooks/useFetching";
 
 const PostIdPage = () => {
   const params = useParams();
+  const [post, setPost] = React.useState({});
 
+  const [fetchPostById, isLoading, postError] = useFetching(async () => {
+    const response = await PostService.getById(params.id);
+    setPost(response.data);
+  });
+
+  React.useEffect(() => {
+    fetchPostById();
+  }, []);
+  console.log(post);
   return (
     <div>
-      <h2>вы открыли страницу поста c ID = {params.id}</h2>
+      <br />
+      <h3>вы открыли страницу поста c ID = {params.id}</h3>
+      <br />
+      <hr />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <br />
+          <strong>
+            {post.id}. {post.title}{" "}
+          </strong>{" "}
+          <br />
+          <br />
+          {post.body}
+        </div>
+      )}
+      <h3>комментарии:</h3>
     </div>
   );
 };
