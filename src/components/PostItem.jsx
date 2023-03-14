d7e8a14d21267e677069e4dabd961f1593327116
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-03-14 21:19:38 +0300

    corrected PostItem

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React from "react";
 import MyButton from "../components/UI/button/MyButton";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const PostItem = (props) => {
-    const navigate = useNavigate();
-    console.log(navigate);
-    const onDeletePost = () => {
-        props.deletePost(props.id)
-    }
-    return (
-        <div className="post">
-            <div className="post__content">
-                
-                <div className="post__text">
-                    <strong>{props.post.id} {props.post.title}</strong> <br />
-                    {props.post.body}
-                </div>
-                <div className="post__btn">
-                    <MyButton onClick={()=> navigate(`/posts/${props.post.id}`)}>открыть</MyButton>
-                    <MyButton onClick={onDeletePost}>удалить</MyButton>
-                </div>
-            </div>
+  const navigate = useNavigate();
+  const onDeletePost = () => {
+    props.deletePost(props.id);
+  };
+  return (
+    <div className="post">
+      <div className="post__content">
+        <div className="post__text">
+          <strong>
+            {props.post.id} {props.post.title}
+          </strong>{" "}
+          <br />
+          {props.post.body}
+        </div>
+        <div className="post__btn">
+          <MyButton onClick={() => navigate(`/posts/${props.post.id}`)}>
+            открыть
+          </MyButton>
+          <MyButton onClick={onDeletePost}>удалить</MyButton>
+        </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default PostItem;
