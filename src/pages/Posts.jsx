7bbf3ca39840082bbdbb70080da3240de74c15f1
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-03-16 11:02:43 +0300

    added endless scroll

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -11,8 +11,6 @@ import { useFetching } from "../hooks/useFetching";
 import { getPageCount } from "../utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
 
-
-
 function Posts() {
   const [posts, setPosts] = React.useState([]);
   const [modal, setModal] = React.useState(false);
@@ -21,11 +19,13 @@ function Posts() {
   const [limit, setLimit] = React.useState(10);
   const [page, setPage] = React.useState(2);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
+  const lastElement = React.useRef();
+  const observer = React.useRef();
+  console.log(lastElement);
 
   const [fetchPosts, isLoading, postError] = useFetching(async () => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
+    setPosts([...posts, ...response.data]);
     const totalCount = response.headers["x-total-count"];
     setTotalPages(getPageCount(totalCount, limit));
   });
@@ -33,6 +33,19 @@ function Posts() {
   React.useEffect(() => {
     fetchPosts();
   }, [page]);
+
+  React.useEffect(() => {
+    if(isLoading) return;
+    if (observer.current) observer.current.disconnect();
+    var callback = function (entries, observer) {
+      if (entries[0].isIntersecting && page < totalPages) {
+        console.log("div ****");
+        setPage(page + 1);
+      }
+    };
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(lastElement.current);
+  }, [isLoading]);
 
   const addNewPost = (newPost) => {
     setPosts((prev) => [...prev, newPost]);
@@ -57,20 +70,14 @@ function Posts() {
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
       {postError && <h2> произошла ошибка${postError}</h2>}
-      {isLoading ? (
-        <Loader style={{ margin: "0 auto" }}>---</Loader>
-      ) : (
-        <PostList
-          posts={sortedAndSearchedPosts}
-          title="Список постов JS"
-          deletePost={deletePost}
-        />
-      )}
-      <Pagination
-        totalPages={totalPages}
-        page={page}
-        setPage={setPage}
+      <PostList
+        posts={sortedAndSearchedPosts}
+        title="Список постов JS"
+        deletePost={deletePost}
       />
+      <div ref={lastElement} style={{ height: 50, background: "red" }}></div>
+      {isLoading && <Loader style={{ margin: "0 auto" }}>---</Loader>}
+      <Pagination totalPages={totalPages} page={page} setPage={setPage} />
     </div>
   );
 }
