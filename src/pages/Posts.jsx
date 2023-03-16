249d5aec531227a074b86263d05172b65af536c1
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-03-16 12:01:26 +0300

    added Myselect to set limit

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -11,14 +11,15 @@ import { useFetching } from "../hooks/useFetching";
 import { getPageCount } from "../utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
 import { useObserver } from "../hooks/useObserver";
+import MySelect from "../components/UI/select/MySelect";
 
 function Posts() {
   const [posts, setPosts] = React.useState([]);
   const [modal, setModal] = React.useState(false);
   const [filter, setFilter] = React.useState({ sort: "", query: "" });
   const [totalPages, setTotalPages] = React.useState(0);
-  const [limit, setLimit] = React.useState(10);
-  const [page, setPage] = React.useState(2);
+  const [limit, setLimit] = React.useState(5);
+  const [page, setPage] = React.useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
   const lastElement = React.useRef();
 
@@ -31,7 +32,7 @@ function Posts() {
 
   React.useEffect(() => {
     fetchPosts();
-  }, [page]);
+  }, [page, limit]);
 
   useObserver(lastElement, page < totalPages, isLoading, () => {
     setPage(page + 1);
@@ -60,12 +61,24 @@ function Posts() {
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
       {postError && <h2> произошла ошибка${postError}</h2>}
+      <MySelect
+        value={limit}
+        onChange={(value) => setLimit(value)}
+        defaultValue={"Количество постов"}
+        options={[
+          { value: 5, name: "5" },
+          { value: 10, name: "10" },
+          { value: 25, name: "25" },
+          { value: -1, name: "все" },
+        ]}
+      />
       <PostList
         posts={sortedAndSearchedPosts}
         title="Список постов JS"
         deletePost={deletePost}
       />
-      <div ref={lastElement} style={{ height: 50, background: "red" }}></div>
+
+      <div ref={lastElement} style={{ height: 5 }}></div>
       {isLoading && <Loader style={{ margin: "0 auto" }}>---</Loader>}
       <Pagination totalPages={totalPages} page={page} setPage={setPage} />
     </div>
